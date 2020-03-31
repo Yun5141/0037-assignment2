5d2d73b6ab1c9da66fdaf970e726ee38e822fc6c
Yun5141 <yun.fang.17@ucl.ac.uk>
2020-03-31 07:11:22 +0100

    done

--- graphDrawer.py
+++ graphDrawer.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
  
 baselineFile = open('entropy_data_baseline.txt','r')
 baselineData = baselineFile.readlines()
+baselineData = filter(None,map(lambda x: x.strip('\n'), baselineData))
+print(len(baselineData))
 
 selfAlgoFile = open('entropy_data_selfAlgo.txt','r')
 selfAlgoData = selfAlgoFile.readlines()
+selfAlgoData = filter(None,map(lambda x: x.strip('\n'), selfAlgoData))
+print(len(selfAlgoData))
+
 
 x = range(max([len(baselineData), len(selfAlgoData)]))
  
@@ -13,19 +18,18 @@ fig,ax = plt.subplots()
 plt.xlabel('time')
 plt.ylabel('entropy')
  
-minValue = float(min([min(baselineData), min(selfAlgoData)]))
+minValue = float(min([min(baselineData),min(selfAlgoData)]))
 maxValue = float(max([max(baselineData), max(selfAlgoData)]))
-yticks = range(int(minValue),int(maxValue),1)
-#yticks = range(10,110,10)
+
+yticks = range(int(minValue),int(maxValue),1000)
 ax.set_yticks(yticks)
  
 ax.set_ylim([minValue,maxValue])
 ax.set_xlim([0, len(x)])
  
-plt.plot(x,baselineData,"x-",label="baseline Data")
-plt.plot(x,selfAlgoData,"+-",label="selfAlgo Data")
- 
-"""open the grid"""
+plt.plot(x,baselineData,"x-",label="Baseline Algorithm")
+plt.plot(x,selfAlgoData,"+-",label="Wave Font Detection Algorithm")
+
 plt.grid(True)
  
 plt.legend(bbox_to_anchor=(1.0, 1), loc=1, borderaxespad=0.)
